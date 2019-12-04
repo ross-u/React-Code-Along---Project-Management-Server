738978b97015e48055d849b9ac612bb885830005
Uros Cirkovic <cirkovic.uros.srb@gmail.com>
2019-12-04 17:10:11 +0100

    Update mongoose connection URI

--- app.js
+++ app.js
@@ -2,20 +2,23 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const mongoose     = require('mongoose');
+const mongoose = require('mongoose');
 
 const app = express();
 
 // MONGOOSE CONNECTION
 mongoose
-  .connect('mongodb://localhost/project-management-server', {useNewUrlParser: true})
+  .connect('mongodb://localhost:27017/project-management-server', {
+    useNewUrlParser: true,
+  })
   .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`,
+    );
   })
   .catch(err => {
-    console.error('Error connecting to mongo', err)
+    console.error('Error connecting to mongo', err);
   });
-
 
 // MIDDLEWARE SETUP
 app.use(logger('dev'));
@@ -24,13 +27,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 // CORS SETTINGS TO ALLOW CROSS-ORIGIN INTERACTION:
 //  ...
 
 // ROUTES MIDDLEWARE:
 //  app.use('', projectRouter);
 //  app.use('', taskRouter);
-
 
 module.exports = app;
