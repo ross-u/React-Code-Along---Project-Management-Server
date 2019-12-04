6b02b3d0ab41d8d2fdf29ea91ac48315c0f8ae53
Uros Cirkovic <cirkovic.uros.srb@gmail.com>
2019-12-04 18:16:30 +0100

    Update route endpoint comments

--- routes/project-routes.js
+++ routes/project-routes.js
@@ -1,26 +1,11 @@
 //      routes/project-routes.js
 
+// POST '/api/projects'    => to post a new projects
 
-
-
-// POST '/projects'    => to post a new projects
-
-
-
-
-// GET '/projects'		 => to get all the projects
-
-
-
+// GET '/api/projects'		 => to get all the projects
 
 // GET '/api/projects/:id'		 => to get a specific projects
 
-
-
-
 // PUT '/api/projects/:id' 		=> to update a specific project
-
-
-
 
 // DELETE '/api/projects/:id'   => to delete a specific project
