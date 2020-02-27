9b0a0d8eef6c281d95c4a96d29f2d44deb06e87f
ross-u <cirkovic.uros.srb@gmail.com>
2020-02-27 17:48:06 +0100

    Add dotenv config require

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 
